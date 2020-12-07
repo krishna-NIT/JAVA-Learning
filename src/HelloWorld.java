b75af38cd98b424a3e0dca08fa86b8088104ce33
ShriKrushna Bhagwat <krish30jan@gmail.com>
2020-12-07 09:21:24 +0530

    variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("Hello World \n");
         System.out.println("Check Done");
-        System.out.print("Ok Google");
+        int a = 20;
+        int b= 10;
+        System.out.println("Ok Google");
+        System.out.print(a+b);
+
+
     }
 }

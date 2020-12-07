b92c36de25e77b11e7e9d20046fb523534b73da4
ShriKrushna Bhagwat <krish30jan@gmail.com>
2020-12-07 10:20:14 +0530

    Casting assignment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World \n");
-        System.out.println("Check Done");
-        int a = 20;
-        int b= 10;
-        System.out.println("Ok Google");
-        System.out.print(a+b);
-
-
+        byte byteNumber = 20;
+        short ShortNumber = 30;
+        int no = 10;
+        long var = 50000 + (10 * (byteNumber + ShortNumber + no));
+        System.out.println(var);
     }
 }

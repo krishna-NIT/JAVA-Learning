d7a42b34cc4da9bd72dc4933f56129055f2c5117
ShriKrushna Bhagwat <krish30jan@gmail.com>
2020-12-07 10:24:32 +0530

    Need of Casting assignment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
         short ShortNumber = 30;
         int no = 10;
         long var = 50000 + (10 * (byteNumber + ShortNumber + no));
+        short num = (short)(1000+10*(byteNumber + ShortNumber + no));
         System.out.println(var);
     }
 }

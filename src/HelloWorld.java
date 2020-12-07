4ee8d7483dd90bc62eab7d414f3b3f5b8de3825a
ShriKrushna Bhagwat <krish30jan@gmail.com>
2020-12-07 09:04:25 +0530

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World \n");
+        System.out.println("Check Done");
+        System.out.print("Ok Google");
+    }
+}

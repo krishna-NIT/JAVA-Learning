cad3fab04fab727f186246e5877dde26ca6b5708
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-02-08 08:36:05 +0530

    Missing No Problem

--- src/com/revision/MissingNo.java
+++ src/com/revision/MissingNo.java
@@ -1,75 +1,34 @@
 package com.revision;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.LinkedList;
 
 public class MissingNo {
     static int[] missingNumbers(int[] arr, int[] brr) {
-        int[] farr = new int[brr.length-1];
+        LinkedList<Integer> lfina = new LinkedList<Integer>();
         for (int i =0;i<brr.length;i++){
             int count = 0;
             int k =0;
-            int countiindex =0;
             for (int j =0;j<arr.length;j++){
                 if (brr[i]!=arr[j]){
                     count++;
-                    countiindex =j;
                 }
             }
-            if (count<brr.length-1){
-                farr[k]=countiindex;
-                k++;
+            if (count==arr.length){
+               lfina.add(brr[i]);
             }
         }
+
+        System.out.println(lfina);
+        int[] farr = new int[lfina.size()];
+            for (int i = 0;i<lfina.size();i++){
+                arr[i] = lfina.get(i);
+            }
+        System.out.println(farr);
         return farr;
     }
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] arr = new int[n];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
-
-        int m = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] brr = new int[m];
-
-        String[] brrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < m; i++) {
-            int brrItem = Integer.parseInt(brrItems[i]);
-            brr[i] = brrItem;
-        }
-
-        int[] result = missingNumbers(arr, brr);
-
-        for (int i = 0; i < result.length; i++) {
-            bufferedWriter.write(String.valueOf(result[i]));
-
-            if (i != result.length - 1) {
-                bufferedWriter.write(" ");
-            }
-        }
-
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+    public static void main(String[] args){
+        int[] arr = {7,2,5,3,5,3};
+        int[] brr = {7,2,5,4,6,3,5,3};
+        System.out.println(missingNumbers(arr,brr));
     }
 }

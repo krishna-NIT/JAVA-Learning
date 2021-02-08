b20c5fd80355a0b2259f7504e226f7675eebf1fe
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-02-08 10:11:31 +0530

    PhoneBook Dictionary

--- src/com/revision/MissingNo.java
+++ src/com/revision/MissingNo.java
@@ -1,6 +1,9 @@
 package com.revision;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
 
 public class MissingNo {
     static int[] missingNumbers(int[] arr, int[] brr) {
@@ -11,6 +14,7 @@ public class MissingNo {
             for (int j =0;j<arr.length;j++){
                 if (brr[i]!=arr[j]){
                     count++;
+                    arr[j]=-1;
                 }
             }
             if (count==arr.length){
@@ -21,14 +25,34 @@ public class MissingNo {
         System.out.println(lfina);
         int[] farr = new int[lfina.size()];
             for (int i = 0;i<lfina.size();i++){
-                arr[i] = lfina.get(i);
+                farr[i] = lfina.get(i);
             }
-        System.out.println(farr);
         return farr;
     }
     public static void main(String[] args){
-        int[] arr = {7,2,5,3,5,3};
-        int[] brr = {7,2,5,4,6,3,5,3};
-        System.out.println(missingNumbers(arr,brr));
+//        int[] arr = {203,204,205,206,207,208,203,204,205,206};
+//        int[] brr = {203,204,204,205,206,207,205,208,203,206,205,206,204};
+//        int[] result = missingNumbers(arr,brr);
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+        Map<String,Integer> phonels = new HashMap<String, Integer>();
+            for(int i = 0; i < n; i++){
+                String name = in.next();
+                int phone = in.nextInt();
+                // Write code here
+                phonels.put(name,phone);
+            }
+            int i = phonels.size();
+            while (i>0){
+                String s = in.next();
+                // Write code here
+                if (phonels.get(s)!=null) {
+                    System.out.println(s + "=" + phonels.get(s));
+                }else {
+                    System.out.println("Not found");
+                }
+                i--;
+            }
+            in.close();
     }
 }

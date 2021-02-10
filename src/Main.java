1012fb05d1a21af68792296640b64de63e90778c
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-02-10 20:20:49 +0530

    y

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
         public int[] twoSum(int[] nums, int target) {
             int[] ans = new int[2];
@@ -11,5 +16,20 @@ public class Main {
             }
             return ans;
         }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ls = new List<List<Integer>>();
+
+
+            for (int i=0;i<nums.length;i++){
+                for (int b=i+1;b<nums.length;b++){
+                     for (int k=b+1;k<nums.length;k++){
+                            if (nums[i]+nums[b]+nums[k]==0){
+
+                    }
+                }
+            }
+        }
+    }
+
 
 }

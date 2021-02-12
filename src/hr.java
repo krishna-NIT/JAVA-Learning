c15fffafd5e3461396d5da8acfec240b92c328a6
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-02-12 16:59:05 +0530

    Resolved #420abg52

--- src/hr.java
+++ src/hr.java
@@ -1,21 +1,46 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class hr {
-    public static void main(String []argh)
-    {int a =Integer.MIN_VALUE;
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String input=sc.next();
-            System.out.println(met(input));
-        }
-
+        int[] arr = {0,0,0,1,1,1,2,2,2};
+        System.out.println(hasGroupsSizeX(arr));
     }
-    public boolean hasGroupsSizeX(int[] deck) {
-        if(deck.length%2!=0){
-            return false;
-        }
 
+
+    public static boolean hasGroupsSizeX(int[] deck) {
+        LinkedList<Integer> factor = new LinkedList<Integer>();
+
+        for (int i =2;i<deck.length;i++){
+            if (deck.length%i==0) {
+                factor.add(i);
+            }
+        }
+        int bha=0;
+        System.out.println(factor);
+        for (int k = 0;k<factor.size();k++) {
+            for (int i = 0; i < deck.length; i++) {
+                int count = 0;
+                for (int j = 0; j < deck.length; j++) {
+                    if (deck[i] == deck[j]) {
+                        count++;
+                    }
+                }
+                if ((count % factor.get(k)) != 0) {
+                    return false;
+                }else{
+                    bha++;
+                }
+
+            }
+            System.out.println(bha);
+            if(bha == (deck.length)){
+                return true;
+            }
+        }
+        return true;
     }
     public static boolean met(String input){
         Stack<Character> stack = new Stack<Character>();

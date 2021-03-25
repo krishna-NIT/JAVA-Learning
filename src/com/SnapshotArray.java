c24c62921bae1b9800d54369b6b6e1b106cd4fca
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-03-25 09:27:08 +0530

    25 updated

--- src/com/SnapshotArray.java
+++ src/com/SnapshotArray.java
@@ -94,7 +94,7 @@ public class SnapshotArray {
         int maxlen = 0;
         for (int i = 0;i<arr.length-2;i++){
             for (int j = i+2;j<arr.length;j++){
-                int max = ;
+                int max = i+j ;
             }
         }
         return maxlen;
@@ -107,7 +107,124 @@ public class SnapshotArray {
         }
 
     }
+    public int divide(int dividend, int divisor) {
+    if (dividend == -2147483648 && divisor == -1){
+        return 2147483647;
+    }
+    if (dividend == 0){
+        return 0;
+    }
+        int temp = 0;
+    if (dividend<0 && divisor>0){
+        temp =1;
+        dividend*=(-1);
+    }
+    if (dividend>0 && divisor<0){
+        temp = 1;
+        divisor*=(-1);
+    }
+    if (dividend<0 && divisor<0){
+        dividend*=(-1);
+        divisor*=(-1);
+    }
+    int ans = 0;
+    if (divisor == 1){
+        ans = dividend;
+    }else {
+        ans = dividend / divisor;
+    }
+        if (divisor == 1 && dividend == 1){
+            ans = dividend;
+        }
+        if (temp == 1 && ans<0){
+            return ans;
+        }
+    if (temp == 1){
+        return ((-1)*ans);
+    }else {
+        return ans;
+    }
+    }
 
+    public int minOperations(String[] logs) {
+    LinkedList ls = new LinkedList();
+    int point = 0;
+    ls.add("Main");
+    for (int i  = 0;i<logs.length;i++){
+        if (logs[i].length()-1>=0) {
+            if (logs[i].charAt(logs[i].length() - 1) == '/') {
+                if (logs[i].length()-2 >=0) {
+                    if (logs[i].charAt(logs[i].length() - 2) != '.') {
+                        ls.add(logs[i].substring(0, logs[i].length() - 2));
+                        point++;
+                    } else {
+                        if (logs[i].length() - 3 >= 0) {
+                            if (logs[i].charAt(logs[i].length() - 3) == '.' && !ls.isEmpty() && point>=1) {
+                                ls.pop();
+                                point--;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return point;
+    }
+
+    public String makeGood(String s) {
+        LinkedList<Character> lschar = new LinkedList<Character>();
+        for (int i = 0;i<s.length();i++){
+            lschar.add(s.charAt(i));
+        }
+
+        int temp = 0;
+        while (temp<=(s.length()+1/2)) {
+            for (int i = 0; i < lschar.size()-1; i++) {
+                    int upascii = (int) lschar.get(i + 1);
+                    int lowscii = (int) lschar.get(i);
+
+
+                if (lowscii >= 97 && lowscii <= 122 && lowscii - upascii == 32) {
+                        lschar.remove(i);
+                        lschar.remove(i);
+                }
+
+                if (upascii >= 97 && upascii <= 122 && upascii-lowscii == 32) {
+                        lschar.remove(i);
+                        lschar.remove(i);
+                }
+
+            }
+            temp++;
+        }
+
+        String ans = "";
+        for (int i = 0;i<lschar.size();i++){
+            ans += lschar.get(i);
+        }
+        return ans;
+    }
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+    int[][] darr = new int[R][C];
+    int[][] arr = new int[1][2];
+    return darr;
+    }
+
+    void prank(long[] a, int n)
+    {   
+        int bha = 0;
+        LinkedList ls = new LinkedList();
+        for (int i = 0;i<a.length && bha<a.length;i++){
+            int temp = (int) a[i];
+            ls.add(a[temp]);
+            bha++;
+        }
+        for (int i = 0;i<a.length;i++){
+            System.out.print(ls.get(i)+" ");
+        }
+
+    }
 
     public boolean circularArrayLoop(int[] nums) {
         int temp = 0;

9a3acc051951e536ce0f9db52e71f65111060bd4
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-08-01 00:38:22 +0530

    up

--- src/Circle.java
+++ src/Circle.java
@@ -1,12 +1,48 @@
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Circle {
 double radius;
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int r= scan.nextInt();
-        System.out.println(isPalindrome(r));
+        int[] arr = new int[r];
+        for (int i =0;i<r;i++){
+            arr[i] = scan.nextInt();
+        }
+        int k = scan.nextInt();
+        addToArrayForm(arr,k);
+    }
+
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+        int actualn = 0;
+        int pow = num.length-1;
+        int i = 0;
+        while (pow>=0){
+            actualn += (num[i]*Math.pow(10,pow));
+            i++;
+            pow--;
+        }
+        System.out.println(actualn);
+        int finaln = actualn + k;
+        System.out.println(finaln);
+        List<Integer> lsfinal = new Stack<Integer>();
+        while (finaln > 0){
+            int rem = finaln % 10;
+            finaln /= 10;
+            lsfinal.add(rem);
+        }
+        System.out.println(lsfinal);
+        List<Integer> lsf = new LinkedList<Integer>();
+        int j = lsfinal.size()-1;
+        while (j>=0) {
+            lsf.add(lsfinal.remove(j));
+            j--;
+        }
+        System.out.println(lsf);
+        return lsf;
     }
 
     public static boolean isPalindrome(int x) {

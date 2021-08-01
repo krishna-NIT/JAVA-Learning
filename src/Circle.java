d7af05bf2713c947b9cac16de48542fc564ccf1f
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-08-01 11:44:16 +0530

    bacl

--- src/Circle.java
+++ src/Circle.java
@@ -16,21 +16,77 @@ double radius;
         addToArrayForm(arr,k);
     }
 
+    public String longestCommonPrefix(String[] strs) {
+        String com = "";
+        int j = 0;
+        int count = 0;
+        for (int i = 0;i<strs.length;i++){
+            char temp = strs[i].charAt(j);
+
+        }
+        return com;
+    }
+
+    public int maxArea(int[] height) {
+        int maxa = 0;
+        for (int i = 0 ; i<height.length-1;i++){
+            for (int j = i+1 ;j<height.length;j++){
+                maxa = Math.max(maxa, (Math.min(height[i], height[j])) * (j - i));
+            }
+        }
+        return maxa;
+    }
+
+    public static int min0f2(int a, int b){
+        if (a < b){
+            return a;
+        }else {
+            return b;
+        }
+    }
     public static List<Integer> addToArrayForm(int[] num, int k) {
-        int actualn = 0;
+        List<Integer> lsfinal = new LinkedList<Integer>();
+        for (int i = num.length-1;i>=0;i--){
+            lsfinal.add(0,(num[i]+k) % 10);
+            k = ( num[i] + k) / 10;
+        }
+        System.out.println(lsfinal);
+        while (k>0){
+            lsfinal.add(0,k%10);
+            k /= 10;
+        }
+        return lsfinal;
+    }
+
+    public List<Integer> addToArrayForm2(int[] A, int K) {
+        List<Integer> res = new LinkedList<>();
+        for (int i = A.length - 1; i >= 0; --i) {
+            res.add(0, (A[i] + K) % 10);
+            K = (A[i] + K) / 10;
+        }
+        System.out.println(res);
+        while (K > 0) {
+            res.add(0, K % 10);
+            K /= 10;
+        }
+        return res;
+    }
+    public static List<Integer> addToArrayForm3(int[] num, int k) {
+        double actualn = 0;
         int pow = num.length-1;
         int i = 0;
         while (pow>=0){
             actualn += (num[i]*Math.pow(10,pow));
+            System.out.println(actualn);
             i++;
             pow--;
         }
-        System.out.println(actualn);
-        int finaln = actualn + k;
+        System.out.println(" Final Actual Number "+actualn);
+        double finaln = actualn + k;
         System.out.println(finaln);
         List<Integer> lsfinal = new Stack<Integer>();
         while (finaln > 0){
-            int rem = finaln % 10;
+            int rem = (int) finaln % 10;
             finaln /= 10;
             lsfinal.add(rem);
         }

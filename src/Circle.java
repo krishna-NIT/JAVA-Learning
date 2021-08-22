56168de4e31da2ae597b04b3385d271d8d59c0af
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-08-22 17:10:55 +0530

    ls

--- src/Circle.java
+++ src/Circle.java
@@ -24,7 +24,6 @@ public class Circle {
         }else {
             return false;
         }
-        return isHappy(newnum);
     }
 
     public String convertToTitle(int columnNumber) {
--- src/com/revision/LinkeeddList.java
+++ src/com/revision/LinkeeddList.java
@@ -1,4 +1,30 @@
 package com.revision;
 
 public class LinkeeddList {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+
+    Node head;
+
+    public static LinkeeddList lsinsert(LinkeeddList ls, int val){
+        Node newnode = new Node(val);
+        if (ls.head == null){
+            ls.head = newnode;
+        }else {
+            Node last = ls.head;
+
+            while (last.next != null){
+                last = last.next;
+            }
+            last.next = newnode;
+        }
+        return ls;
+    }
 }

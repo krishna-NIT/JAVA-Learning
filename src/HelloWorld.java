043cfd906bb0432505b9ef6b592f9f41e401cf93
ShriKrushna Bhagwat <krish30jan@gmail.com>
2022-08-02 13:36:28 +0530

    amz question solved

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,336 @@
 import com.TreeNode;
 
 import java.util.*;
+import java.util.ArrayList;
 
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String inp = scan.nextLine();
-        System.out.println(moveHyphen2(inp,inp.length()));
-        
+        int n = scan.nextInt();
+        List<String> ls = new ArrayList<>();
+        for (int i = 0;i<n;i++){
+            String inp = scan.next();
+            ls.add(inp);
+        }
+        System.out.println(max_greyness((ls)));
+    }
+
+    public static int get_min_trips(int[] arr){
+        List<Integer> distinct = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0;i<arr.length;i++){
+            if (map.containsKey(arr[i])){
+                map.replace(arr[i],map.get(arr[i])+1);
+            }else {
+                map.put(arr[i],1);
+                distinct.add(arr[i]);
+            }
+        }
+
+        int count = 0;
+        for (int i = 0;i<distinct.size();i++){
+            int response = no_of_trip(map.get(distinct.get(i)),0);
+            if (response == -1){
+                return -1;
+            }
+            count += response;
+        }
+        return count;
+    }
+    public static int no_of_trip(int weight, int count){
+        if (weight == 0){
+            return count;
+        }
+        if (weight == 1){
+            return -1;
+        }else if (weight == 2){
+            return 1+count;
+        }else if (weight == 3){
+            return 1+count;
+        }
+
+        int rem = count%3;
+        int div = count/3;
+        count += div;
+
+
+        return no_of_trip(rem,count);
+    }
+
+
+
+    public static int max_greyness(List<String> strls){
+        int[] rsum = new int[strls.size()];
+        int[] csum = new int[strls.get(0).length()];
+
+        int rmax = 0;
+        int cmax = 0;
+
+        for (int i = 0;i<strls.size();i++){
+            int rowsum = 0;
+            for (int j = 0 ;j<strls.get(0).length();j++){
+                if (strls.get(i).charAt(j) == '1'){
+                    rowsum += 1;
+                }
+            }
+            rsum[i] = rowsum;
+            if (rowsum > rmax){
+                rmax = rowsum;
+            }
+        }
+
+        for (int i = 0;i<strls.get(0).length();i++){
+            int colsum = 0;
+            for (int j = 0 ;j<strls.size();j++){
+                if (strls.get(j).charAt(i) == '1'){
+                    colsum += 1;
+                }
+            }
+            csum[i] = colsum;
+            if (colsum > cmax){
+                cmax = colsum;
+            }
+        }
+        int max_grey = ((2*(cmax + rmax)) - (strls.size() + strls.get(0).length()));
+        return max_grey;
+    }
+
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        HashMap<String, Integer> map = new HashMap<>();
+        ArrayList<String> arrls = new ArrayList<String>();
+        for (int i = 0;i<words1.length;i++){
+            map.put(words1[i], 0);
+            int count = 0;
+            for (int j = 0;j<words2.length;j++){
+                if (words1[i].contains(words2[i])){
+                    count++;
+                }
+            }
+            if (count == words2.length){
+                arrls.add(words1[i]);
+            }
+        }
+        return arrls;
+    }
+
+    HashMap<Character, String> mapk = new HashMap<>();
+    public List<String> letterCombinations(String digits) {
+        List<String> ls = new ArrayList<>();
+        if (digits.length() == 0){
+            return ls;
+        }
+        mapk.put('2',"abc");
+        mapk.put('3',"def");
+        mapk.put('4',"ghi");
+        mapk.put('5',"jkl");
+        mapk.put('6',"mno");
+        mapk.put('7',"pqrs");
+        mapk.put('8',"tuv");
+        mapk.put('9',"wxyz");
+        backTrack(0,digits,ls,"");
+        return ls;
+    }
+    public void backTrack(int i, String digits, List<String> arr, String str){
+        if (i == digits.length()){
+            arr.add(str);
+        }else {
+            //String numString = map.get(digits.charAt(i));
+//            for (int j = 0;j<numString.length();j++){
+//                String s = str;
+//                s += numString.charAt(j);
+//                backTrack(i+1,digits,arr,s);
+//            }
+        }
+    }
+
+    public static boolean isSafe(int[][] arrstatus, int x, int y){
+        if (x < arrstatus.length && y < arrstatus.length && arrstatus[x][y] == 1){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean ratinmaze(int[][] arr, int x, int y, int[][] sol){
+        if (x ==  arr.length-1 && y == arr.length-1){
+            sol[x][y] = 1;
+            return true;
+        }
+        if (isSafe(arr,x,y)){
+            sol[x][y] = 1;
+            if (ratinmaze(arr,x+1,y,sol)){
+                return true;
+            }else if (ratinmaze(arr,x,y+1,sol)){
+                return true;
+            }else {
+                sol[x][y] = 0; // backtrack
+                return false;
+            }
+        }else {
+            sol[x][y] = 0;
+            return false;
+        }
+
+    }
+    public char findTheDifference(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (map.containsKey(t.charAt(i))){
+                if (map.get(t.charAt(i)) == 0){
+                    return t.charAt(i);
+                }else {
+                    map.replace(t.charAt(i), map.get(t.charAt(i))-1);
+                }
+            }else {
+                return t.charAt(i);
+            }
+        }
+        return 'd';
+    }
+
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        int index = 0;
+        for (int i=0;i<s.length();i++){
+            if (!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),index);
+                index++;
+            }
+        }
+
+        index = 0;
+        int[] iloc = new int[s.length()];
+        for (int i=0;i<s.length();i++) {
+            iloc[i] = map.get(s.charAt(i));
+        }
+
+        HashMap<Character, Integer> word = new HashMap<>();
+        for (int i=0;i<t.length();i++){
+            if (!word.containsKey(t.charAt(i))){
+                word.put(t.charAt(i),index);
+                index++;
+            }
+        }
+        for (int i=0;i<s.length();i++) {
+            if (iloc[i] != word.get(t.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0;i<nums.length;i++){
+            if (map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+                if (map.get(nums[i]) > 1){
+                    return true;
+                }
+            }else {
+                map.put(nums[i],1);
+            }
+        }
+        return false;
+    }
+
+
+    public boolean isHappy(int n) {
+        if(n == 1){
+            return true;
+        }
+        LinkedList<Integer> ls = new LinkedList<Integer>();
+        ls.add(n);
+        while (n>1){
+            ArrayList<Integer> arls = new ArrayList<>();
+            int num = n;
+            int sum = 0;
+            while (num > 0){
+                int digit = num % 10;
+                num /= 10;
+                sum += (digit*digit);
+            }
+            n = sum;
+            System.out.println(n);
+            if (ls.contains(n)){
+                return false;
+            }
+            ls.add(n);
+            if(n == 1){
+                return true;
+            }
+        }
+
+        if (n==1){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static Integer maxfreqno(int[] arr){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxno = Integer.MIN_VALUE;
+        int freq = 0;
+        for (int i = 0;i<arr.length;i++){
+            if (map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }else {
+                map.put(arr[i], 1);
+            }
+            if (map.get(arr[i]) > freq){
+                maxno = arr[i];
+                freq = map.get(arr[i]);
+            }
+        }
+        return maxno;
+    }
+
+    public static int maxFrequencyNumber(int[] arr){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        ArrayList<Integer> arls = new ArrayList<>();
+        for (int i =0;i<arr.length;i++){
+            if (hm.containsKey(arr[i])){
+                int precount = hm.get(arr[i]);
+                hm.replace(arr[i],precount+1);
+            }else {
+                hm.put(arr[i],1);
+                arls.add(arr[i]);
+            }
+        }
+        int maxno = 0;
+        int maxrepeat = Integer.MIN_VALUE;
+        for (int i =0;i<arls.size();i++){
+            if (maxrepeat < hm.get(arls.get(i))){
+                maxrepeat = hm.get(arls.get(i));
+                maxno = arls.get(i);
+            }
+        }
+        return maxno;
+    }
+
+    public static ArrayList<Integer> remodub(int arr[]){
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        for (int i = 0;i<arr.length;i++){
+            if (!map.containsKey(arr[i])){
+                ans.add(arr[i]);
+                map.put(arr[i], true);
+            }
+        }
+        return ans;
     }
 
     public static int arcessium(List<Integer> ls, int target){
